729a51c4b671b7c423f0f104dc4ea7a38164312b
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-11 00:09:12 -0400

    implement grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Bedroom from './components/Bedroom';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="app">
+      <div className="app-container">
+        <div className="app-header">
+          <Header />
+        </div>
+        <div className="app-page">
+          <Bedroom />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Bedroom.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Bedroom.css'
+import './page.css'
+
+function Bedroom() {
+    return (
+        <div className="bedroom">
+            <div className="page-container">
+                <div className="page-title"><h1>This is Bedroom Title!</h1></div>
+                <div className="bedroom-container">
+                    <h3>This is Bedroom component!</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Bedroom
