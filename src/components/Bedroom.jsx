f0c7d901245975c3ee3d954bbc5fd195ccf0b77b
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-13 23:54:12 -0400

    update page container

--- src/components/Bedroom.jsx
+++ src/components/Bedroom.jsx
@@ -6,7 +6,12 @@ function Bedroom() {
     return (
         <div className="bedroom">
             <div className="page-container">
-                <div className="page-title"><h1>This is Bedroom Title!</h1></div>
+                <div className="page-title">
+                    <h2>Bedroom inspiration for every taste</h2>
+                </div>
+                <div className="page-description">
+                    <p>Not sure which bed or bedroom furniture is right for you? You can browse this gallery of fully-furnished bedrooms to find the looks that suit your style.</p>
+                </div>
                 <div className="bedroom-container">
                     <h3>This is Bedroom component!</h3>
                 </div>
