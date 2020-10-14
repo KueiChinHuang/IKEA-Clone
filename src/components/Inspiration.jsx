e7650e034e6aca7f9994e93ada833d7fc4ea2d95
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-14 11:53:29 -0400

    update classnames in css

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -4,7 +4,13 @@ import './Inspiration.css'
 function Inspiration() {
     return (
         <div className="inspiration">
-            <h4 className="content">Inspiration</h4>
+            <div className="shoppable-image">
+                <img src="bedroom1.webp" alt="bedroom1" />
+                <div className="shoppable-image__area">
+                    <a className="shoppable-image__dot"></a>
+                    <div className="shoppable-image__tag" role="tooltip"></div>
+                </div>
+            </div>
             
         </div>
     )
