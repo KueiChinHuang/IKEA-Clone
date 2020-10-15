174c3db00d47071b326dc5ce1c2db379f41b7cbb
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-15 14:22:13 -0400

    loop tooltip works

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -1,20 +1,58 @@
 import React from 'react'
 import './Inspiration.css'
 import ReactTooltip from 'react-tooltip';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 function Inspiration() {
+
+    const items = [
+        {
+            pid: 1,
+            title: "REGOLIT",
+            description: "Pendant lamp shade, white17 ¾ (45 cm)",
+            price: "4.99",
+            position_top: "21%",
+            position_left: "60.5%"
+        },
+        {
+            pid: 2,
+            title: "SKURUP",
+            description: "Wall lamp, black",
+            price: "14.99",
+            position_top: "29.6%",
+            position_left: "25.5%"
+        }
+    ]
+    
+
+    let itemsHTML = []
+
+    for (let item of items) {
+        itemsHTML.push(
+            <div className="inspiration-image__area" style={{top: item.position_top, left: item.position_left}} >                        
+            <ReactTooltip id={item.title} className="tooltip" place="right"  effect="solid" arrowColor="transparent" clickable="true" >                            
+                <div className="tooltip-container">
+                    <h4 className="tooltip-title">{item.title}</h4>
+                    <sapn className="tooltip-description">{item.description}</sapn>
+                    <div className="tooltip-arrow">
+                        <ArrowForwardIosIcon  fontSize="small" />
+                    </div>
+                    <span className="tooltip-price">{item.price}</span>
+                </div>                            
+            </ReactTooltip>                        
+            <a className="inspiration-image__dot" data-tip data-for={item.title}></a>
+        </div>
+        );
+    }
+
+     
+
     return (
         <div className="inspiration">
             <div className="inspiration-container">
                 <div className="inspiration-image">
                     <img className="inspiration-image__img" src="bedroom1.webp" alt="bedroom1" />
-                    <div className="inspiration-image__area" style={{top: "21%", left: "60.5%"}}>
-                        <a className="inspiration-image__dot" data-tip></a>
-                        <ReactTooltip className="inspiration-image__tag" place="right"  effect="solid" arrowColor="transparent" clickable="true" delayUpdate={1000}>
-                            <p>tooltip</p>
-                        </ReactTooltip>
-
-                    </div>
+                    {itemsHTML}
                 </div>
             </div>
             
