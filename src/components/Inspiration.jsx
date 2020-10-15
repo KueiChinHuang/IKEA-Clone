6bbb9fb5e69018ec91f8c91bcc7453dc2b037441
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-14 23:44:07 -0400

    implement react-tooltip

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Inspiration.css'
+import ReactTooltip from 'react-tooltip';
 
 function Inspiration() {
     return (
         <div className="inspiration">
-            <div className="shoppable-image">
-                <img src="bedroom1.webp" alt="bedroom1" />
-                <div className="shoppable-image__area">
-                    <a className="shoppable-image__dot"></a>
-                    <div className="shoppable-image__tag" role="tooltip"></div>
+            <div className="inspiration-container">
+                <div className="inspiration-image">
+                    <img className="inspiration-image__img" src="bedroom1.webp" alt="bedroom1" />
+                    <div className="inspiration-image__area" style={{top: "21%", left: "60.5%"}}>
+                        <a className="inspiration-image__dot" data-tip></a>
+                        <ReactTooltip className="inspiration-image__tag" place="right"  effect="solid" arrowColor="transparent" clickable="true" delayUpdate={1000}>
+                            <p>tooltip</p>
+                        </ReactTooltip>
+
+                    </div>
                 </div>
             </div>
             
