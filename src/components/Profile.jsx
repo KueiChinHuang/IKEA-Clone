be57812f300d98473729566ff352f9a3ae3f7f75
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-23 18:34:05 -0400

    update profile page style

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./Profile.css";
 import { useStateValue } from "../StateProvider";
 import { auth } from "../firebase";
-import { Redirect, useHistory } from "react-router-dom";
+import { Link, Redirect, useHistory } from "react-router-dom";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 
 function Profile() {
   const history = useHistory();
@@ -18,10 +19,41 @@ function Profile() {
 
   return (
     <div className="profile">
-      {!user && <Redirect to="/login" />}
-      <h1>Hello {user && user?.providerData[0].uid}</h1>
-      <div className="profile-login" onClick={handleAuthentication}>
-        Log out
+      <div className="profile-container">
+        {!user && <Redirect to="/login" />}
+
+        <div className="profile-title">
+          <h1>Hello {user && user?.providerData[0].uid}</h1>
+          <div>
+            Need to change account?
+            <span onClick={handleAuthentication}>Log out</span>
+          </div>
+        </div>
+        <div className="profile-card-container">
+          <Link to="/orders">
+            <div className="profile-card">
+              <div className="profile-card-content">
+                <p>My Orders</p>
+                <p>View your order history</p>
+              </div>
+              <ArrowForwardIcon />
+            </div>
+          </Link>
+          <div className="profile-card">
+            <div className="profile-card-content">
+              <p>Bonus Meal</p>
+              <p>Buy 10 eligible meals and get the next one free</p>
+            </div>
+            <ArrowForwardIcon />
+          </div>
+          <div className="profile-card">
+            <div className="profile-card-content">
+              <p>The IKEA Sell-Back Program</p>
+              <p>Re-circulate. Re-decorate</p>
+            </div>
+            <ArrowForwardIcon />
+          </div>
+        </div>
       </div>
     </div>
   );
