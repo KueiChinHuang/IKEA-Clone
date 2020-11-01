2172bd30902bd5fb766018d9fe6e76e5e6eac4e1
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-11-01 18:06:02 -0500

    use another firebase account

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZYu_Lb7SaPmdsZqGIPjCNMkb2VyYlDOk",
-  authDomain: "ikea-clone-8d049.firebaseapp.com",
-  databaseURL: "https://ikea-clone-8d049.firebaseio.com",
-  projectId: "ikea-clone-8d049",
-  storageBucket: "ikea-clone-8d049.appspot.com",
-  messagingSenderId: "700019070549",
-  appId: "1:700019070549:web:3dbc21d4d596046efae6dc",
-  measurementId: "G-WN51DFTLPM"
+  apiKey: "AIzaSyD7dosGMedjjGfDSsNA21wUgfyfdDALfQo",
+  authDomain: "ikea-clone-5327a.firebaseapp.com",
+  databaseURL: "https://ikea-clone-5327a.firebaseio.com",
+  projectId: "ikea-clone-5327a",
+  storageBucket: "ikea-clone-5327a.appspot.com",
+  messagingSenderId: "687551606949",
+  appId: "1:687551606949:web:10044dbd5cf46fb6305d4c",
+  measurementId: "G-M4TXMLNMY7"
 };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
